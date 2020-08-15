d9a437d20c009a05cc261f52b265030991efc942
chongkwongyu2 <chongkwongyu2@gmail.com>
2020-08-16 01:00:47 +0800

    QuickBacktest script: Bulk backtesting and bulk stats & results

--- QuickBacktest.py
+++ QuickBacktest.py
@@ -31,6 +31,66 @@ class BacktestResult:
         plt.plot(df['x'], df['close'])
         plt.title(f'Entry-exit points')
 
+    def stats(self):
+        stats = dict()
+        pv = self.result_df.set_index('datetime')['pv']
+        tk = self.result_df['adjclose'] if 'adjclose' in self.result_df.columns else self.result_df['close']
+        ret = np.log(pv).diff()
+        tk_ret = np.log(tk).diff()
+        stats['N'] = pv.shape[0] - 1
+        stats['Sharpe'] = qs.stats.sharpe(pv)
+        stats['Sortino'] = qs.stats.sortino(pv)
+        stats['CAGR %'] = (qs.stats.cagr(pv)) * 100
+        stats['Cum Return %'] = (pv.iloc[-1] / pv.iloc[0] - 1) * 100
+        stats['Cum Return (Stock) %'] = (tk.iloc[-1] / tk.iloc[0] - 1) * 100
+        stats['Daily Ret %'] = (qs.stats.avg_return(pv)) * 100
+        stats['Daily Vol %'] = (qs.stats.volatility(pv) / (251 ** (1 / 2))) * 100
+        stats['Monthly Ret %'] = (stats['Daily Ret %'] * 21)
+        stats['Monthly Vol %'] = (qs.stats.volatility(pv) / (21 ** (1 / 2))) * 100
+        stats['Annual Ret %'] = (stats['Daily Ret %'] * 251)
+        stats['Annual Vol %'] = (qs.stats.volatility(pv)) * 100
+        stats['Win Days %'] = (qs.stats.win_rate(pv)) * 100
+        stats['Max Drawdown %'] = (qs.stats.max_drawdown(pv)) * 100
+        stats['Daily VAR %'] = (qs.stats.var(pv)) * 100
+        stats['Beta'] = np.cov(ret.dropna(), tk_ret.dropna())[0][1] / np.var(tk_ret.dropna())
+        stats['Alpha'] = stats['Cum Return %'] - stats['Cum Return (Stock) %']
+        stats['No Trades'] = self.trade_df.shape[0]
+        return stats
+
+
+class BacktestResults:
+    def __init__(self, backtest_results: dict):
+        self.all_results = backtest_results
+
+    def ticker_report(self, ticker, benchmark):
+        self.all_results[ticker].report(benchmark=benchmark)
+
+    def ticker_plot(self, ticker):
+        self.all_results[ticker].plot()
+
+    def ticker_stats(self, ticker):
+        self.all_results[ticker].stats()
+
+    def stats(self):
+        tk_stats = dict()
+        for tk in self.all_results.keys():
+            tk_stats[tk] = self.all_results[tk].stats()
+        return np.round(pd.DataFrame(tk_stats), 2)
+
+    def portfolio_result(self, benchmark, allocations=None):
+        import webbrowser
+        portfolio_pv = self.make_portfolio(allocations=allocations)['pv']
+        html = 'backtest result.html'
+        qs.reports.html(portfolio_pv, benchmark, output=html, title=f'Backtest result')
+        webbrowser.open(html)
+
+    def make_portfolio(self, allocations=None):
+        df = pd.concat([x.result_df.set_index('datetime')['pv'] for x in self.all_results.values()])
+        df.columns = self.all_results.keys()
+        df = df.fillna(method='bfill')
+        allocations = [1 / len(self.all_results)] * len(self.all_results) if allocations is None else allocations
+        return pd.DataFrame({'datetime': df.index, 'pv': np.dot(df, allocations)}).set_index('datetime')
+
 
 def standard_specified_signal(row, cur_cash, cur_qty, action_price, action_dt):
     return row['signal'].upper(), row['qty']
@@ -41,9 +101,15 @@ def percent_invested_signal(row, cur_cash, cur_qty, action_price, action_dt):
         return "PASS", 0
     ev = cur_qty * action_price
     pv = cur_cash + ev
-    required_qty = int(pv * float(row['%']) / action_price)
+    try:
+        percent = float(row['%'])
+        if percent > 1 or percent < -1:
+            raise Exception()
+    except Exception:
+        print(f'Invalid % invested {row["%"]}, return PASS signal')
+        return "PASS", 0
 
-
+    required_qty = int(pv * percent / action_price)
     action_qty = required_qty - cur_qty
     if action_qty == 0:
         return "PASS", 0
@@ -53,9 +119,8 @@ def percent_invested_signal(row, cur_cash, cur_qty, action_price, action_dt):
         return "SHORT", abs(action_qty)
 
 
-
-
-def backtest(df, capital, buy_at_open=True, spread: float = 0.0, fee_mode: str = 'FIXED:0', signal_func=standard_specified_signal):
+def run_backtest(df, capital, buy_at_open=True, bid_ask_spread: float = 0.0, fee_mode: str = 'FIXED:0',
+                 signal_func=standard_specified_signal):
     # Very quick backtesting function, give it a df that contains columns: datetime, open, close, signal, qty
     # signal column: 'PASS, LONG, EXIT LONG, SHORT, COVER SHORT, COVER AND LONG, EXIT AND SHORT, LIQUIDATE'
     # qty: quantity value (float or int) to trade, or "ALL"
@@ -82,18 +147,21 @@ def backtest(df, capital, buy_at_open=True, spread: float = 0.0, fee_mode: str =
         mode, fee = fee_mode.split(':')
         fee = float(fee)
         if mode.upper() == 'FIXED':
-            def cal_fee(qty):
+            def cal_fee(qty, price):
                 return fee
         elif mode.upper() == 'QTY':
-            def cal_fee(qty):
+            def cal_fee(qty, price):
                 return abs(int(qty)) * fee
+        elif mode.upper() == 'PERCENT':
+            def cal_fee(qty, price):
+                return abs(int(qty)) * price * fee
         else:
             raise Exception()
 
     except Exception:
         print(f'Invalid fee mode {fee_mode}, using zero fees')
 
-        def cal_fee(qty):
+        def cal_fee(qty, price):
             return 0.0
 
     for id, row in df.iterrows():
@@ -107,20 +175,21 @@ def backtest(df, capital, buy_at_open=True, spread: float = 0.0, fee_mode: str =
         action_price = row[action_price_label]
         action_dt = row[action_datetime]
 
-        signal, input_qty = signal_func(row=row, cur_cash=cur_cash, cur_qty=cur_qty,action_price=action_price,action_dt=action_dt)
+        signal, input_qty = signal_func(row=row, cur_cash=cur_cash, cur_qty=cur_qty, action_price=action_price,
+                                        action_dt=action_dt)
         signal = signal.upper()
         input_qty = str(input_qty).upper()
 
         if signal == 'PASS':
-            continue
+            pass
 
         elif signal == 'LONG' and cur_cash > 0:
-            action_price = action_price * (1 + spread)
+            action_price = action_price * (1 + bid_ask_spread)
             max_qty = int(cur_cash / action_price)
             bot_qty = max_qty if input_qty == 'ALL' else min(max_qty, abs(int(input_qty)))
 
             cash_proceeds = -bot_qty * action_price
-            txn_fee = cal_fee(bot_qty)
+            txn_fee = cal_fee(bot_qty, action_price)
 
             cur_cash = cur_cash + cash_proceeds - txn_fee
             cur_qty += bot_qty
@@ -128,47 +197,47 @@ def backtest(df, capital, buy_at_open=True, spread: float = 0.0, fee_mode: str =
             trades.loc[action_dt] = ['BUY', action_price, bot_qty, cash_proceeds, txn_fee]
 
         elif signal == 'EXIT LONG' and cur_qty > 0:
-            action_price = action_price * (1 - spread)
+            action_price = action_price * (1 - bid_ask_spread)
 
             exit_qty = cur_qty
 
             cash_proceeds = cur_qty * action_price
-            txn_fee = cal_fee(exit_qty)
+            txn_fee = cal_fee(exit_qty, action_price)
 
             cur_cash = cur_cash + cash_proceeds - txn_fee
             cur_qty = 0
             trades.loc[action_dt] = ['SELL', action_price, exit_qty, cash_proceeds, txn_fee]
 
         elif signal == 'SHORT':
-            action_price = action_price * (1 - spread)
+            action_price = action_price * (1 - bid_ask_spread)
 
             ev = cur_qty * action_price
             pv = cur_cash + ev
             min_short = -int(pv / action_price)
-            short_qty = abs(int(input_qty))
+
+            short_qty = (cur_qty - min_short) if input_qty == 'ALL' else abs(int(input_qty))
             if cur_qty - short_qty < min_short:
                 short_qty = cur_qty - min_short
 
-
             cash_proceeds = short_qty * action_price
-            txn_fee = cal_fee(short_qty)
+            txn_fee = cal_fee(short_qty, action_price)
             cur_cash = cur_cash + cash_proceeds - txn_fee
             cur_qty -= short_qty
             trades.loc[action_dt] = ['SELL', action_price, short_qty, cash_proceeds, txn_fee]
 
         elif signal == 'COVER SHORT' and cur_qty < 0:
-            action_price = action_price * (1 + spread)
+            action_price = action_price * (1 + bid_ask_spread)
 
             bot_qty = cur_qty * -1
 
-            cash_proceeds = - bot_qty * action_price
-            txn_fee = cal_fee(bot_qty)
+            cash_proceeds = cur_qty * action_price
+            txn_fee = cal_fee(bot_qty, action_price)
             cur_cash = cur_cash + cash_proceeds - txn_fee
             cur_qty += bot_qty
             trades.loc[action_dt] = ['BUY', action_price, bot_qty, cash_proceeds, txn_fee]
 
         elif signal == 'COVER AND LONG' and cur_qty <= 0:
-            action_price = action_price * (1 + spread)
+            action_price = action_price * (1 + bid_ask_spread)
 
             pv = cur_qty * action_price + cur_cash
             max_qty = int(pv / action_price)
@@ -176,22 +245,22 @@ def backtest(df, capital, buy_at_open=True, spread: float = 0.0, fee_mode: str =
             bot_qty = -1 * cur_qty + long_qty
 
             cash_proceeds = -action_price * bot_qty
-            txn_fee = cal_fee(bot_qty)
+            txn_fee = cal_fee(bot_qty, action_price)
 
             cur_cash = cur_cash + cash_proceeds - txn_fee
             cur_qty += bot_qty
             trades.loc[action_dt] = ['BUY', action_price, bot_qty, cash_proceeds, txn_fee]
 
         elif signal == 'EXIT AND SHORT' and cur_qty >= 0:
-            action_price = action_price * (1 - spread)
+            action_price = action_price * (1 - bid_ask_spread)
 
             pv = cur_qty * action_price + cur_cash
             max_qty = int(pv / action_price)
-            short_qty = max_qty if input_qty == 'ALL' else min(max_qty, int(input_qty))
+            short_qty = max_qty if input_qty == 'ALL' else min(max_qty, abs(int(input_qty)))
             sell_qty = -short_qty - cur_qty
 
             cash_proceeds = - sell_qty * action_price
-            txn_fee = cal_fee(sell_qty)
+            txn_fee = cal_fee(sell_qty, action_price)
             cur_cash = cur_cash + cash_proceeds - txn_fee
 
             cur_qty += sell_qty
@@ -199,14 +268,14 @@ def backtest(df, capital, buy_at_open=True, spread: float = 0.0, fee_mode: str =
 
         elif signal == 'LIQUIDATE' and cur_qty != 0:
             if cur_qty > 0:
-                action_price = action_price * (1 - spread)
+                action_price = action_price * (1 - bid_ask_spread)
             else:
-                action_price = action_price * (1 + spread)
+                action_price = action_price * (1 + bid_ask_spread)
 
             action_qty = -cur_qty
             cash_proceeds = cur_qty * action_price
 
-            txn_fee = cal_fee(action_qty)
+            txn_fee = cal_fee(action_qty, action_price)
             cur_cash = cur_cash + cash_proceeds - txn_fee
 
             cur_qty = 0
@@ -220,10 +289,8 @@ def backtest(df, capital, buy_at_open=True, spread: float = 0.0, fee_mode: str =
 
     completed_df = df.copy()
     completed_df = completed_df.merge(records.reset_index(), on=['datetime'], how='right')
-    completed_df['ev'] = completed_df['quantity'] * completed_df['close']
-    completed_df['pv'] = completed_df['ev'] + completed_df['cash']
-    completed_df['ev'] = completed_df['ev'].astype(float)
-    completed_df['pv'] = completed_df['pv'].astype(float)
+    completed_df['ev'] = (completed_df['quantity'] * completed_df['close']).astype(float)
+    completed_df['pv'] = (completed_df['ev'] + completed_df['cash']).astype(float)
     completed_df['interval return'] = np.log(completed_df['pv']).diff()
     completed_df['datetime'] = pd.to_datetime(completed_df['datetime'])
     trades = trades.reset_index()
@@ -231,27 +298,91 @@ def backtest(df, capital, buy_at_open=True, spread: float = 0.0, fee_mode: str =
     return BacktestResult(result_df=completed_df, trade_df=trades)
 
 
-if __name__ == '__main__':
-    from examples import research_tools
+def create_signal(df, strategy_func, signal_mode='qty', max_rows=None):
+    df = df.copy()
+    signal_list = list()
+    value_list = list()
+    max_rows = df.shape[0] if max_rows is None else max_rows
+    for i in range(0, df.shape[0]):
+        partial_df = df.iloc[:i + 1].iloc[-max_rows:]
+        signal, value = strategy_func(partial_df)
+        signal_list.append(signal)
+        value_list.append(value)
+    df['signal'] = signal_list
+    df[signal_mode] = value_list
+    return df
 
-    df = research_tools.download_data('K_DAY', 'HK.00700')
-    df['sma16'] = df['close'].rolling(16).mean()
-    df['sma32'] = df['close'].rolling(32).mean()
-    df['dif'] = df['sma16'] - df['sma32']
-    df['pre_dif'] = df['dif'].shift(1)
-    df = df.dropna()
-    signal = list()
-    for id, row in df.iterrows():
+
+def backtest(tickers, strategy_func, init_capital=1000000, signal_mode='qty', buy_at_open=True,
+             bid_ask_spread: float = 0.0,
+             fee_mode: str = 'FIXED:0', signal_func=standard_specified_signal, start_date=None, end_date=None,
+             max_rows=None):
+    backtest_result_dict = dict()
+    print('Downloading data from Yahoo...')
+    dfs = yh.Stocks(tickers).prices()
+
+    for tk in tickers:
+        if tk not in dfs.keys():
+            print(f'Pass backtesting of {tk}...')
+            continue
+        df = dfs[tk]
+        if start_date:
+            df = df.loc[df['datetime'] >= start_date]
+        if end_date:
+            df = df.loc[df['datetime'] <= start_date]
+        df = create_signal(df=df, strategy_func=strategy_func, max_rows=max_rows, signal_mode=signal_mode)
+        print(f'Backtesting {tk}...')
+        backtest_result = run_backtest(df, capital=init_capital, buy_at_open=buy_at_open, bid_ask_spread=bid_ask_spread,
+                                       fee_mode=fee_mode, signal_func=signal_func)
+        backtest_result_dict[tk] = backtest_result
+    return BacktestResults(backtest_results=backtest_result_dict)
+
+
+if __name__ == '__main__':
+    import Yahoo as yh
+
+
+    # df = yh.Stock('0700.HK').price()
+    # df = df.loc[df['datetime'] >= '2015-01-02']
+    #
+    #
+    # df = create_signal(df=df, strategy_func=sma_crossover_signal, max_rows=33)
+    # backtest_result = run_backtest(df, capital=100000, buy_at_open=False)
+    #
+    #
+    # df = yh.Stock('1299.HK').price()
+    # df = df.loc[df['datetime'] >= '2015-01-02']
+    #
+    # def sma_crossover_signal(df):
+    #     df['sma16'] = df['adjclose'].rolling(16).mean()
+    #     df['sma32'] = df['adjclose'].rolling(32).mean()
+    #     df['dif'] = df['sma16'] - df['sma32']
+    #     df['pre_dif'] = df['dif'].shift(1)
+    #     row = df.iloc[-1]
+    #     if row['dif'] > 0 and row['pre_dif'] <= 0:
+    #         return 'COVER AND LONG', 'ALL'
+    #
+    #     elif row['dif'] < 0 and row['pre_dif'] >= 0:
+    #         return 'EXIT AND SHORT', 'ALL'
+    #     else:
+    #         return 'PASS', ''
+    #
+    # df = create_signal(df=df, strategy_func=sma_crossover_signal, max_rows=33)
+    # backtest_result2 = run_backtest(df, capital=100000, buy_at_open=False)
+
+    def sma_crossover_signal(df):
+        df['sma16'] = df['adjclose'].rolling(16).mean()
+        df['sma32'] = df['adjclose'].rolling(32).mean()
+        df['dif'] = df['sma16'] - df['sma32']
+        df['pre_dif'] = df['dif'].shift(1)
+        row = df.iloc[-1]
         if row['dif'] > 0 and row['pre_dif'] <= 0:
-            signal.append('COVER AND LONG')
+            return 'COVER AND LONG', 'ALL'
 
         elif row['dif'] < 0 and row['pre_dif'] >= 0:
-            signal.append('EXIT AND SHORT')
+            return 'EXIT AND SHORT', 'ALL'
         else:
-            signal.append('PASS')
-
-    df['signal'] = signal
-    df['qty'] = "ALL"
-    backtest_result = backtest(df, capital=100000, signal_func=standard_specified_signal)
+            return 'PASS', ''
 
 
+    result = backtest(tickers=['0700.HK', '1299.HK'], strategy_func=sma_crossover_signal)
