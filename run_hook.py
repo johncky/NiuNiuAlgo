bbe58547bd61fd728e8eb08e5f4a12a155a463a4
chongkwongyu2 <chongkwongyu2@gmail.com>
2020-08-30 21:05:00 +0800

    run script mod

--- run_hook.py
+++ run_hook.py
@@ -5,6 +5,12 @@ if __name__ == '__main__':
     if cwd not in sys.path:
         sys.path.append(cwd)
 
+    if sys.platform == 'linux2':
+        import ctypes
+
+        libc = ctypes.cdll.LoadLibrary('libc.so.6')
+        libc.prctl('PR_SET_NAME ', 'FutuHook', 0, 0, 0)
+
     import FutuAlgo
 
     # edit your own
