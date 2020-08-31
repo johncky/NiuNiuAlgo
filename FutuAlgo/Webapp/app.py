5afb89438d3d8486dd4ccdf88bfceec95f8d1fb9
chongkwongyu2 <chongkwongyu2@gmail.com>
2020-08-31 23:39:23 +0800

    server run scripts

--- FutuAlgo/Webapp/app.py
+++ FutuAlgo/Webapp/app.py
@@ -526,7 +526,8 @@ class WebApp:
 
         # with open('templates/index.html') as file:
         #     template = Template(file.read())
-        template = Template(template)
+        print(template.read())
+        template = Template(template.read())
         t = response.html(template.render(data=data, url_for=self.app.url_for))
         return t
 
