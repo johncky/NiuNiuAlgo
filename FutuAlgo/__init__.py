b2b524f236679439c3bf612fdc6a9b7bd63a70ef
chongkwongyu2 <chongkwongyu2@gmail.com>
2020-08-31 21:53:42 +0800

    fix pending order api

--- FutuAlgo/__init__.py
+++ FutuAlgo/__init__.py
@@ -1,3 +1,8 @@
 from FutuAlgo.Algo import BaseAlgo, CandlestickStrategy, Backtest
 from FutuAlgo.FutuHook import FutuHook
 from FutuAlgo.Webapp.app import WebApp
+
+
+
+
+from FutuAlgo.examples.SMA_crossover import SMACrossover
